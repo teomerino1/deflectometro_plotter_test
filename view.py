7211af40f78ca2cec3b1df6e73f758b3f360c5c8
alejomoyano <alejomoyano16@gmail.com>
2023-07-07 12:11:00 -0300

    No pierdo datos en las interfaces. Tengo control para moverme

--- view.py
+++ view.py
@@ -26,14 +26,16 @@ class View():
         ft_ntry = None
         fh_ntry = None
         fc_ntry = None
-        
-
         #Se crean los objetos Plot y Config como atributos de view 
         self.Config = config.Config(root, self.go_to_plot1_from_config)
         self.Plot = plot.Plot(root,self.go_to_config, self.go_to_plot_2_from_plot_1)
         self.Plot2 = plot_2.Plot2(root,self.go_to_plot_1_from_plot_2)
         self.is_plotting = False
         self.start(root)
+
+        self.first_time_plot=True
+        
+        self.first_time_plot2=True
        
        
       
@@ -62,23 +64,50 @@ class View():
 
         root.geometry(f"{screen_width}x{screen_height}")
 
-    # Metodo que borra el frame Config y abre el Plot1
+
+     # Metodo que borra el frame Config y abre el Plot1
     def go_to_plot1_from_config(self):
-        self.Config.close()
-        self.Plot.show(0)
-        # self.Plot.grid_plot1()
         self.is_plotting = True
+
+        if(self.first_time_plot):
+
+            self.first_time_plot=False
+
+            self.Config.close()
+
+            self.Plot.show(0)
+
+        else:
+            self.Config.close()
+
+            self.Plot.show(1)
+
+    # # Metodo que borra el frame Config y abre el Plot1
+    # def go_to_plot1_from_config(self):
+    #     self.Config.close()
+    #     self.Plot.show(0)
+    #     # self.Plot.grid_plot1()
+    #     
 
     # Metodo que borra el Plot 1 y abre el de Config
     def go_to_config(self):
         self.Plot.close()
         self.Config.show()
 
-
     # Metodo que borra el Plot 1 y abre el Plot 2
     def go_to_plot_2_from_plot_1(self):
-        self.Plot.close()
-        self.Plot2.show(0)
+        if(self.first_time_plot2):
+            self.first_time_plot2=False
+            self.Plot.close()
+            self.Plot2.show(0)
+        else:
+            self.Plot.close()
+            self.Plot2.show(1)
+
+    # # Metodo que borra el Plot 1 y abre el Plot 2
+    # def go_to_plot_2_from_plot_1(self):
+    #     self.Plot.close()
+    #     self.Plot2.show(0)
 
     # Metodo que borra el Plot 2 y abre el Plot 1
     def go_to_plot_1_from_plot_2(self):
@@ -101,4 +130,3 @@ class View():
     
     def on_plot(self):
         return self.is_plotting;
-      
