33101113e6552d466f2f3fccd031873c8a3c31f4
alejomoyano <alejomoyano16@gmail.com>
2023-09-08 13:40:14 -0300

    Añado images

--- main.py
+++ main.py
@@ -57,11 +57,8 @@ def process_data(Reporter,View,Data):
             if(Reporter.get_puesto_change()==1 or a==muestras or View.get_reset()==1):
                 
                 print("Identifico reset")
-
                 View.set_state("Detenido")
-               
                 print("Vuelvo a empezar")
-                
                 View.set_data_ready(value=0)
                 # sleep(1)
                 obtain_data(Reporter,View,Data)
@@ -80,20 +77,24 @@ def process_data(Reporter,View,Data):
         print(cantidad)
         
         if(Reporter.get_puesto_change()==0):
-            if(a>=100):
-                b=b+1
-                if(b==10):
-                    b=0
-                    update_bar_thread = Thread(target=update_defl_one,args=(Data,View,10))
-                    update_bar_thread.daemon=True
-                    update_bar_thread.start()
-            else:
-                # View.set_state("Graficando bars...")
-                update_bar_thread = Thread(target=update_defl_one,args=(Data,View,1))
-                update_bar_thread.daemon=True
-                update_bar_thread.start()
+            # if(a>=100):
+            #     b=b+1
+            #     if(b==10):
+            #         b=0
+            #         update_bar_thread = Thread(target=update_defl_one,args=(Data,View,10))
+            #         update_bar_thread.daemon=True
+            #         update_bar_thread.start()
+            # else:
+            #     # View.set_state("Graficando bars...")
+            #     update_bar_thread = Thread(target=update_defl_one,args=(Data,View,1))
+            #     update_bar_thread.daemon=True
+            #     update_bar_thread.start()
+            
+            update_bar_thread = Thread(target=update_defl_one,args=(Data,View,1))
+            update_bar_thread.daemon=True
+            update_bar_thread.start()
                 
-            if(cantidad%10 == 0):
+            if(cantidad%2 == 0):
                 update_all_thread = Thread(target=update_all,args=(Data,View,grupos))
                 update_all_thread.daemon=True 
                 update_all_thread.start()
